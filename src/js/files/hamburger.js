160af0fe12ff2156f0b17ba51c1cd7bf6c11f0cc
Ceglius <Ceglius 74723353+Ceglius@users.noreply.github.com>
2023-02-27 22:12:27 +0000

    responsive header

--- src/js/files/hamburger.js
+++ src/js/files/hamburger.js
@@ -2,18 +2,25 @@ function burgerMenu() {
     const burger = document.querySelector('.burger')
     const menu = document.querySelector('.menu')
     const body = document.querySelector('body')
+    const overlay = document.querySelector('.overlay');
+    const logo = document.querySelector('.logo > a');
     
     burger.addEventListener('click', () => {
         if (!menu.classList.contains('active')) {
             menu.classList.add('active')
             burger.classList.add('active')
             body.classList.add('locked')
+            overlay.classList.add('active')
+            logo.classList.add('active')
            
         } else {
             menu.classList.remove('active')
             burger.classList.remove('active')
             body.classList.remove('locked')
+            overlay.classList.remove('active')
+            logo.classList.remove('active')
         }
+    
     })
     
     window.addEventListener('resize', () => {
@@ -21,6 +28,8 @@ function burgerMenu() {
             menu.classList.remove('active')
             burger.classList.remove('active')
             body.classList.remove('locked')
+            overlay.classList.remove('active')
+            logo.classList.remove('active')
         }
     })
 }
